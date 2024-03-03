9d2cc4d6c4556e80dbbca3550ee18918abea79f6
Harun23H <147631933+Harun23H@users.noreply.github.com>
2024-03-03 15:32:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const wrapper = document.getElementById("container");
+
+const createCard = (items) => {
+  let card = document.createElement("div");
+  card.classList.add("country");
+
+  cardContent = ` <div class="card">
+    <div class="cardImg">
+        <img src= ${items.flags.svg} alt="">
+    </div>
+    <div class="cardText">
+        <p>${items.name.common}</p>
+        <small>Population: <span> ${items.population}</span></small>
+        <small>Region: <span> ${items.region}</span></small>
+        <small>Capital: <span> ${items.capital}</span></small>
+    </div>
+</div>`;
+  card.innerHTML = cardContent;
+  wrapper.appendChild(card);
+};
+
+const fetchData = async () => {
+  try {    
+    let api_data = "https://restcountries.com/v3.1/all";
+
+    const response = await fetch(api_data);
+    const data = await response.json();
+
+
+    data.map((items) => {
+      return createCard(items);
+    });
+  } catch (error) {
+    let errMessage = document.createElement("p");
+    errMessage.classList.add("errorMessage");
+
+    errMessage.innerHTML = `error message is: ${error}`;
+    wrapper.appendChild(errMessage);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", fetchData);
